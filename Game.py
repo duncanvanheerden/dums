c6572d597c0af8e2a9ab8103fc1aa2d61c8b09a4
weperra022 <weperra022@student.wethinkcode.co.za>
2023-03-06 08:20:09 +0200

    Game selecting module

---
+++ Game.py
@@ -0,0 +1,22 @@
+def selecting_game_mode():
+    
+    while True:
+        
+        users_input = input("(1)Dums in terminal or (2)Dums in pygame: ")
+        
+        
+        if users_input == "1":
+            import dums_in_terminal.dums 
+            # break
+            
+
+        elif users_input == "2":
+            import dums_in_pygame.dums_pygame
+            # break
+        else:
+            pass
+            
+            
+            
+if __name__ == '__main__':
+    selecting_game_mode()
