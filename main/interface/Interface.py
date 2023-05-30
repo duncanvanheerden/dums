3aba25e9fda2b8aae6fe19afbb57ad62c81fd53b
Wesley-Perrang <wes3perrang@gmail.com>
2023-05-30 10:28:13 +0200

    added main menu with start and quit buttons

--- main/interface/Interface.py
+++ main/interface/Interface.py
@@ -1,5 +1,8 @@
 import pygame as pg
+import Button.Button as button
+import sys
 
+fps = 60
 
 class Interface():
 
@@ -16,16 +19,41 @@ class Interface():
 
         # Set the title of the window
         pg.display.set_caption("My Pygame Window")
+        
+        #load button images
+        start_img = pg.image.load('./Button/img/start_button.png').convert_alpha()
+        exit_img = pg.image.load('./Button/img/exit_button.png').convert_alpha()
 
+        #create button instances
+        start_button = button.Button(200, 100, start_img, 0.5)
+        exit_button = button.Button(200, 300, exit_img, 0.5)
+        
+        start_menu = "main"
+        
         # Enter the main event loop
         running = True
         while running:
+            # Fill the screen with white color
+            screen.fill((255, 255, 255))
+            
+            if start_menu == "main":
+                # Draw the button
+                if start_button.draw(screen):
+                    print("play")
+                    start_menu = "play"
+                    pass
+                
+                if exit_button.draw(screen):
+                    running = False
+            
+            if start_menu == "play":
+                # Draw the button
+                    print("play")
+                    # start_menu = "play"
+            
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     running = False
-
-            # Fill the screen with white color
-            screen.fill((255, 255, 255))
 
             # Add your code for drawing or updating the window here
 
