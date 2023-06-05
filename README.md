a993a02e0600a79db23aed1eb4b21729fa8ba9ec
Wesley-Perrang <wes3perrang@gmail.com>
2023-06-05 11:31:44 +0200

    feature(interface): Added more buttons,Readme: Added features,how to play,rules

--- README.md
+++ README.md
@@ -1,8 +1,51 @@
-# dums
+# Dums
 
-A wacky take on the original dominoes game.
+A wacky take on the original dominoes game.This repository contains a Dominoes game implemented in [Python]. The game allows players to play unique way of playing Dominoes against a computer opponent. It provides a user-friendly interface and implements the South African rules of playing.
 
-Running Unit Tests
+## Features
+
+- Play against a computer opponent
+- Interactive and intuitive user interface
+- Standard rules of Dominoes
+- Score tracking and game statistics
+- Customizable settings (e.g., difficulty level, game mode)
+- Save and load game functionality
+- Sound effects and animations (optional)
+
+## How to Play
+
+1. The game starts with a set of domino tiles shuffled and each player being dealt a hand of tiles.
+
+2. The players take turns placing one of their tiles on the board. The tile must be placed adjacent to an existing tile, matching the number of pips on the adjacent sides.
+
+3. If a player cannot make a valid move, they must draw from the boneyard until they either have a playable tile or the boneyard is empty.
+
+4. The game continues until one of the players has no more tiles or neither player can make a move.
+
+5. The player who played all their tiles or has the fewest remaining tiles wins the round. Points are awarded based on the sum of the pips on the opponent's remaining tiles.
+
+6. The game can be played in multiple rounds, and the player with the highest total score at the end wins the game.
+
+7. Enjoy playing Dominoes!
+
+## Rules
+
+The Dominoes game follows the standard rules of play. Here are some key rules:
+
+- Each tile has two sides, with each side containing a certain number of pips.
+
+- Tiles are placed end-to-end, with matching sides touching each other.
+
+- If a player cannot make a move, they must draw from the boneyard until they have a playable tile or the boneyard is empty.
+
+- If the boneyard is empty and neither player can make a move, the game ends.
+
+- Scoring is based on the sum of pips on the opponent's remaining tiles at the end of each round.
+
+- The game can be played in multiple rounds, and the player with the highest total score at the end wins.
+
+
+## Running Unit Tests
 
 To run the unit tests in this project, make sure you have the following configurations set up:
 Visual Studio Code Settings
--- main/interface/Interface.py
+++ main/interface/Interface.py
@@ -12,33 +12,39 @@ class Interface():
 
     def window(self):
         # Set the dimensions of the window
-        width = 1300
-        height = 600
+        # width = 1200
+        # height = 690
         
         # Get current working directory
         cwd = os.getcwd()
 
         # Create the window display
-        screen = pg.display.set_mode((width, height))
+        screen = pg.display.set_mode((0, 0), pg.FULLSCREEN)
 
         # Set the title of the window
         pg.display.set_caption("My Pygame Window")
         
         #load button images
-        start_img = pg.image.load(os.path.join(cwd, 'main/interface/Button/img/start_btn.png'))
+        start_img = pg.image.load(os.path.join(cwd, 'main/interface/Button/img/start_button.png'))
         rules_img = pg.image.load(os.path.join(cwd, 'main/interface/Button/img/rules.png'))
         singlePlay_img = pg.image.load(os.path.join(cwd, 'main/interface/Button/img/Single_player.png'))
         multiplay_img = pg.image.load(os.path.join(cwd, 'main/interface/Button/img/Multiplayer.png'))
         homescreen_img = pg.image.load(os.path.join(cwd, 'main/interface/Button/img/home_screen.png'))
+        playerVplayer_img = pg.image.load(os.path.join(cwd, 'main/interface/Button/img/1v1_button.png'))
+        playerVplayerVplayer_img = pg.image.load(os.path.join(cwd, 'main/interface/Button/img/1v1_button.png'))
+        player2Vplayer2_img = pg.image.load(os.path.join(cwd, 'main/interface/Button/img/1v1_button.png'))
         exit_img = pg.image.load(os.path.join(cwd, 'main/interface/Button/img/exit_button.png'))
 
         #create button instances
-        start_button = button.Button(200, 0, start_img, 0.5)
-        rules_button = button.Button(200, 200, rules_img, 1.2)
-        singlePlay_button = button.Button(200, 200, singlePlay_img, 1.2)
-        multiplay_button = button.Button(200, 400, multiplay_img, 1.2)
-        homescreen = button.Button(200, 600, homescreen_img, 2)
-        exit_button = button.Button(200, 400, exit_img, 0.5)
+        start_button = button.Button(500, 0, start_img, 0.5)
+        rules_button = button.Button(500, 200, rules_img, 1.2)
+        singlePlay_button = button.Button(500, 200, singlePlay_img, 0.5)
+        multiplay_button = button.Button(500, 400, multiplay_img, 0.5)
+        homescreen = button.Button(500, 600, homescreen_img, 1)
+        playerVplayer_button = button.Button(500, 0, playerVplayer_img, 0.5)
+        playerVplayerVplayer_button = button.Button(500, 200, playerVplayerVplayer_img, 0.5)
+        player2Vplayer2_button = button.Button(500, 400, player2Vplayer2_img, 0.5)
+        exit_button = button.Button(500, 400, exit_img, 0.5)
         
         start_menu = "main"
         
@@ -60,11 +66,13 @@ class Interface():
                     
                 if rules_button.draw(screen):
                     start_menu = "rules"
+                
             
             if start_menu == "play":
                 
                 if singlePlay_button.draw(screen):
-                    print("singleplay")
+                    start_menu = "single_player"
+                
                  
                 if multiplay_button.draw(screen):
                     print("multiplay")
@@ -75,6 +83,17 @@ class Interface():
             if start_menu == "rules":
                 print("rules")
             
+            if start_menu == "single_player":
+                
+                if playerVplayer_button.draw(screen):
+                    print("1v1")
+                
+                if playerVplayerVplayer_button.draw(screen):
+                    print("1v1v1")
+                
+                if player2Vplayer2_button.draw(screen):
+                    print("2v2")
+                        
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     running = False
